73c4090a7c518daadf9465edf9e0bd71edb3d22a
Grandon Smith <grandon.smith@yahoo.com>
2020-09-17 22:43:39 -0400

    removed old js file

--- script.js
+++ script.js
@@ -44,46 +44,24 @@ var store = {
          }, 
     ],
     currentQuestion: 0,
-    score: 0
+    score: 0,
+    startQuiz: false
 };
 
+//-----------------------------ENDING THE QUIZ-------------------------------------
 
-function renderStartScreen() {
-    // runs once at load to render the start screen
-    
-    $('main').append(`
-        <div class="js-question-area">
-        <div class="startpic-box">
-        <img src="https://is2-ssl.mzstatic.com/image/thumb/Purple71/v4/a4/dd/05/a4dd05c6-4e03-312b-b8ed-abdbdadef6c6/source/512x512bb.jpg" class="startpic">
-        </div>
-        <form class="start-form js-start-form">
-            <input id="start-quiz js-start-quiz" type="button" value="Start Quiz!">
-        </form>
-        
-        </div>`);
-};
-
-//------------ SCORE KEEPERS-----------
-
-
-
-function updateScore() {
-    store.score++;
-    $('.score').text(`Score: ${store.score} / 8`);
-    console.log("updateScore ran")
+function tryAgain() {
+    $('main').on('click', '#jsRetakeQuiz', event => {
+        resetCounters()
+        renderQuiz();
+        $('header').addClass('padding')
+    })
 }
-
-function updateCurrentQuestion() {
-    store.currentQuestion++;
-    $('.current-question').text(`Current Question: ${store.currentQuestion + 1} / 8`);
-    console.log("updateCurrentQuestion ran");
+function resetCounters() {
+    store.currentQuestion = 0;
+    store.score = 0;
+    store.startQuiz = false;
 }
-
-//------------ SCORE KEEPERS-----------
-
-
-
-
 
 function scoreScreen() {
     //only runs once at the end of the quiz (shown in the if statement in submitAnswer function)
@@ -91,165 +69,181 @@ function scoreScreen() {
     console.log('score screen ran');
     $('.current-question').hide();
     $('.score').remove();
-        $('main').append(`
+        // $('main').html(
+            return`
             <div class="trackers center scoreDiv">
                 <p><strong>
                     Your Score: ${store.score} / 8
                 <strong></p>
                 <form>
                     <div class="submit-div">
-                        <input id="js-retake-quiz" type="submit" value="Try Again?">
+                        <input id="jsRetakeQuiz" type="button" value="Try Again?">
                     </div>
                 </form>
             </div> 
-        `);
+        `;
+}
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+//-------------------------------DEALING WITH QUESTIONS---------------------------------
+
+
+function nextQuestion() {
+    console.log(store.questions.length)
+    console.log(store.currentQuestion)
+    if (store.startQuiz === true && store.currentQuestion < store.questions.length) {
+        $('header').html(formatQuizTrackers());
+        $('main').html(formatQuestionAndAnswer());
+    } else if (store.startQuiz === true && store.currentQuestion === store.questions.length){
+        $('main').html(scoreScreen());
+    }
 }
 
-function hidePreviousQuestion() {
-    $('.question').remove();
-    console.log("hidePreviousQuestion ran")
-};
- 
-function incorrectAnswer() {
-    alert(`Sorry, the correct answer was ${questions[currentQuestion].correct}`);
-    hidePreviousQuestion();
-    updateCurrentQuestion();
-};
-
-function correctAnswer() {
-    hidePreviousQuestion();
-    updateCurrentQuestion();
-    updateScore();  
+function clickNext() {
+    $('main').on('click', '#next', event => {
+    event.preventDefault();
+    console.log("ran")
+    nextQuestion();
+    })
 }
 
-function submitAnswer() {
-    $('main').on('click', '#question-submit', event => {
+function informAnswerStatus(status) {
+    if (status === 'correct') {
+        return `
+        <div class="center">Correct!</div>
+        <input type="submit" id="next" value="next">
+        `
+    } else if (status === 'incorrect') {
+        return `
+        <div class="center">Incorrect. The answer was ${store.questions[store.currentQuestion].correct}.</div>
+        <input type="submit" id="next" value="next">
+        `
+    }
+}
+
+function showAnswerStatus() {
+    $('main').on('submit', 'form', event => {
         event.preventDefault();
-
-        //assigning variables to be used in the if statement below this and selecting
+        // assigning variables to be used in the if statement below this and selecting
         // the input from the radio buttons to be assessed.
-
-        let correct = store.questions[currentQuestion].correct;
+        let correct = store.questions[store.currentQuestion].correct;
         let selected = $('input:checked');
         let answer = selected.val();
-
-        //Needed a separate if statement to determine if the current question was the last one
-        // and if that was the case, to run the score screen function, rather than render the
-        // next question. I'm sure there's a more efficient way to code this, but for now
-        // this was the best I could come up with. 
-
-        if (store.currentQuestion === (store.questions.length - 1)) {
-            console.log("GOT IT");
-            if (answer === undefined) {
-                alert("Please choose one of the options.");
-            } else if (answer === correct) {
-                correctAnswer();
-                scoreScreen();
-            } else if (answer !== correct) {
-                incorrectAnswer();
-                scoreScreen();
-            } return
-        } else if (store.currentQuestion < (store.questions.length)) {
-            
-            if (answer === undefined) {
-                alert("Please choose one of the options.");
-            } else if (store.currentQuestion < store.questions.length && answer === correct) {
-                alert("Correct!")
-                correctAnswer();
-                renderQuestion();
-            } else if (store.currentQuestion < store.questions.length && answer !== correct) {
-                incorrectAnswer();
-                renderQuestion();
-            } return
-        } 
+        if (answer === correct) {
+            console.log("correct")
+            $('.submit-div').html(informAnswerStatus('correct'))
+            store.score++;
+            store.currentQuestion++; 
+        } else if (answer !== correct) {
+            $('.submit-div').html(informAnswerStatus('incorrect'))
+            store.currentQuestion++;
+        }
     });
-};
+}    
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
+// -----------------------FORMAT SCORE AND CURRENT QUESTION-----------------------------
 
-function removeStartQuizButton() {
-    $('.js-start-form').remove();
-};
-
-function addTrackers() {
-    $('header').append(`
-    <div class="trackers">
-            <p class="current-question">Current Question: 1 / 8</p>
-            <p class="score">Score 0 / 8</span>
-        </div>`
-    );
-};
-
-
-function renderQuestion() {
-    //This is the template to which all questions will be rendered, allowing them to be updated
-    // as the quiz questions progress
-    console.log(`question ${store.currentQuestion + 1} was rendered`);
-    $('.js-question-area').append( `
-    <div class="question">
-        <p class="js-question">
-            ${store.questions[currentQuestion].question}
-        </p>
-        <form>
-            <ul>
-                <li>
-                    <input type="radio" id="answer1" name="answer" value="${store[currentQuestion].answers[0]}" aria-pressed="false">
-                    <label for="answer1">${store[currentQuestion].answers[0]}</label>
-                </li>
-                <li>
-                    <input type="radio" id="answer2" name="answer" value="${store[currentQuestion].answers[1]}" aria-pressed="false">
-                    <label for="answer2">${store[currentQuestion].answers[1]}</label>
-                </li>
-                <li>
-                    <input type="radio" id="answer3" name="answer" value="${store[currentQuestion].answers[2]}" aria-pressed="false">
-                    <label for="answer3">${store[currentQuestion].answers[2]}</label>
-                </li>
-                <li>
-                    <input type="radio" id="answer4" name="answer" value="${store[currentQuestion].answers[3]}" aria-pressed="false">
-                    <label for="answer4">${store[currentQuestion].answers[3]}</label>
-                </li>
-            </ul>
-            <div class="submit-div">
-                <input id="question-submit" type="submit" value="Submit Answer">
-            </div>
-        </form>
-    </div>`
-    );
-};
-
-function moveStartPic() {
-    $('img').remove();
-    $('h1').hide();
-    $('header').prepend(`
+function formatQuizTrackers() {  
+    return`
         <div class="startpic-box">
             <img src="https://is2-ssl.mzstatic.com/image/thumb/Purple71/v4/a4/dd/05/a4dd05c6-4e03-312b-b8ed-abdbdadef6c6/source/512x512bb.jpg" class="startpic">
         </div>
-    `);
+        <div class="trackers">
+            <p class="current-question">Current Question: ${store.currentQuestion + 1} / ${store.questions.length}</p>
+            <p class="score">Score so far: ${store.score} / ${store.currentQuestion}</span>
+        </div>
+        `
+}
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+//---------------formatting question area for each question render--------------
+
+function formatAnswers() {
+    const answersArray = store.questions[store.currentQuestion].answers;
+    let i = 0;
+    let formattedAnswers = '';
+    answersArray.forEach(answer => {
+      formattedAnswers += `
+        <li>
+          <input type="radio" tabindex="${i + 1}" name="answer" id="answer${answer}" value="${answer}" required> 
+          <label for="answer${answer}">${answer}</label>
+        </li>
+      `;
+      i++;
+    });
+    return formattedAnswers;
+  }
+
+function formatQuestionAndAnswer() {
+return `
+    <div class="js-question-area">
+        <div class="question">
+            
+            <fieldset>
+                <legend>${store.questions[store.currentQuestion].question}</legend>
+                <form>
+                    <ul>
+                        ${formatAnswers()}
+                    </ul>
+                    <div class="submit-div">
+                        <input id="question-submit" type="submit" value="Submit Answer">
+                    </div>
+                </form>
+            </fieldset>
+        </div>
+    </div>
+    `
+}
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+//----------------GETS RID OF H1 TITLE AND MOVES PICTURE UP TO REPLACE IT--------------
+function moveStartPic() {
+    $('header').removeClass('padding');
+}
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+
+function beginQuiz() {
+    $('main').on('click','#jsStartQuiz', event => {
+        event.preventDefault();
+        store.startQuiz = true;
+        nextQuestion();
+        moveStartPic();
+    });
 }
 
-function removePadding() {
-    $('header').removeClass('padding');
+function startScreen() {
+    return `
+    <div class="js-question-area">
+        <div class="startpic-box">
+            <img src="https://is2-ssl.mzstatic.com/image/thumb/Purple71/v4/a4/dd/05/a4dd05c6-4e03-312b-b8ed-abdbdadef6c6/source/512x512bb.jpg" class="startpic">
+        </div>
+        <form class="start-form js-start-form">
+            <input id="jsStartQuiz" type="button" value="Start Quiz!">
+        </form>
+    </div>
+    `;
 };
 
-function startQuiz() {
-    //upon clicking start quiz, the button will dissapear, the first question rendered, the socre and current
-    // question trackers will be rendered above the question area and the picture will move up, replacing the 
-    // quiz title.
-    $('main').on('click','.js-start-form', event => {
-        removeStartQuizButton();
-        addTrackers();
-        renderQuestion();
-        moveStartPic();
-        removePadding();
-        
-    });
-};
-
+function renderQuiz() {
+    if (store.startQuiz === false) {
+        $('header').empty();
+        $('main').html(startScreen());
+    } 
+}
 // -----------------Callback function ------------------------
 
 function runQuizApp() {
-    renderStartScreen();
-    startQuiz();
-    submitAnswer();
+    clickNext();
+    renderQuiz();
+    beginQuiz();
+    showAnswerStatus();
+    nextQuestion();
+    tryAgain();
 }
 
 $(runQuizApp());
